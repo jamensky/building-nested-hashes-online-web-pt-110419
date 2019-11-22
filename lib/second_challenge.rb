12a1d0f7549335741c54054089e5153c39e75ff3
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-22 22:24:00 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,11 +3,15 @@ def second_challenge
          :montague => { :patriarch => {},
                         :matriarch => {},
                         :hero => {},
-                        :hero_friends => {}
+                        :hero_friends => []
                       },
-         :capulet => {
-      #your key/value pairs here!
-      }}
+         :capulet => {  :patriarch => {}, 
+                        :matriarch => {}, 
+                        :heroine => {}, 
+                        :heroine_friends []
+                      }
+
+                 }
 
 
 end
